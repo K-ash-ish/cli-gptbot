2cc1c5fd0fb8cd7822417aca73f804f1496fec57
K-ash-ish <kashishsondhiya@gmail.com>
2023-07-20 00:50:17 +0530

    completed CLI chat bot using openAI

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+const { Configuration, OpenAIApi } = require("openai");
+const inquirer = require("inquirer");
+const { createSpinner } = require("nanospinner");
+const chalk = require("chalk");
+require("dotenv").config();
+
+const spinner = createSpinner();
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const gptResponse = async (message) => {
+  spinner.start();
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: message }],
+    });
+    spinner.success();
+    return completion.data.choices[0].message.content;
+  } catch (error) {
+    console.error(error);
+  }
+};
+const yourResponse = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: chalk.greenBright("You: "),
+        name: "yourResponse",
+      },
+    ])
+    .then(async (answer) => {
+      if (answer.yourResponse.length === 0) {
+        console.log("Response was empty");
+        return;
+      } else {
+        const gpt = await gptResponse(answer.yourResponse);
+        console.log(chalk.magentaBright("GPT: ") + gpt + "\n");
+        yourResponse();
+      }
+    });
+};
+console.log(
+  chalk.yellowBright("Welcome to CLI chatbot.") +
+    chalk.redBright(" Ctrl+C to exit.\n")
+);
+yourResponse();
